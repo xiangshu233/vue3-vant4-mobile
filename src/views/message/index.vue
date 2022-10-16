e64431a2661ce2077c4e549bdb0897237608a57b
xiangshu233 <xiangshu233@outlook.com>
2022-10-16 23:01:51 +0800

    v-model practice notes

--- src/views/message/index.vue
+++ src/views/message/index.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
-    <van-cell title="单元格" :is-link="true" />
-    <van-cell title="单元格" :is-link="true" value="内容" />
-    <van-cell title="单元格" is-link arrow-direction="down" value="内容" />
-    <van-cell title="单元格" is-link />
-    <van-cell title="单元格" is-link value="内容" />
-    <van-cell title="单元格" is-link arrow-direction="down" value="内容" />
-    <van-cell :border="false" title="个人信息" :is-link="true" to="/" />
-    <van-cell title="URL 跳转" is-link url="https://github.com" />
-    <van-cell title="路由跳转" is-link to="/" />
+    <p class="text-30px mb-50px"> vue3 v-model 的变化</p>
+    input: <input v-model="name" />
+    <p> {{ name }}</p>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  const name = ref('Alice');
+</script>
 
 <style scoped></style>
