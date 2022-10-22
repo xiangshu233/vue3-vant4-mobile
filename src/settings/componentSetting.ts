702747243a9d9ad5f8715f683c799e265d1478f0
xiangshu233 <xiangshu233@outlook.com>
2022-10-22 10:15:38 +0800

    chore 依赖更新 fix 删除无用代码

--- src/settings/componentSetting.ts
+++ src/settings/componentSetting.ts
@@ -1,20 +1,4 @@
 export default {
-  table: {
-    apiSetting: {
-      // 当前页的字段名
-      pageField: 'current',
-      // 每页数量字段名
-      sizeField: 'size',
-      // 接口返回的数据字段名
-      listField: 'records',
-      // 接口返回总页数字段名
-      totalField: 'pages',
-    },
-    //默认分页数量
-    defaultPageSize: 10,
-    //可切换每页数量集合
-    pageSizes: [10, 20, 30, 40, 50],
-  },
   upload: {
     //考虑接口规范不同
     apiSetting: {
@@ -24,7 +8,7 @@ export default {
       imgField: 'imagePath',
     },
     //最大上传图片大小
-    maxSize: 2,
+    maxSize: 1,
     //图片上传类型
     fileType: ['image/png', 'image/jpg', 'image/jpeg', 'image/gif', 'image/svg+xml'],
   },
