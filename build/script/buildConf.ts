ed747e6e497c27dbf05caa2a95e29a06b375f832
xiangshu233 <xiangshu233@outlook.com>
2024-01-02 17:47:06 +0800

    fix: :bug: 修复 fs-extra 升级版本后 build writeFileSync 报错问题

--- build/script/buildConf.ts
+++ build/script/buildConf.ts
@@ -2,7 +2,7 @@
  * Generate additional configuration files when used for packaging. The file can be configured with some global variables, so that it can be changed directly externally without repackaging
  */
 import { GLOB_CONFIG_FILE_NAME, OUTPUT_DIR } from '../constant';
-import fs, { writeFileSync } from 'fs-extra';
+import fs from 'fs-extra';
 import colors from 'picocolors';
 
 import { getRootPath, getEnvConfig } from '../utils';
@@ -28,7 +28,7 @@ function createConfig(
       });
     `.replace(/\s/g, '');
     fs.mkdirp(getRootPath(OUTPUT_DIR));
-    writeFileSync(getRootPath(`${OUTPUT_DIR}/${configFileName}`), configStr);
+    fs.writeFileSync(getRootPath(`${OUTPUT_DIR}/${configFileName}`), configStr);
 
     console.log(colors.cyan(`✨ [${pkg.name}]`) + ` - configuration file is build successfully:`);
     console.log(colors.gray(OUTPUT_DIR + '/' + colors.green(configFileName)) + '\n');
